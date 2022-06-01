e7584b0280ffdd63a4609055c4624df534e593a3
Migdalis <miglepage@gmail.com>
2022-05-31 20:29:27 -0500

    :sparkles: Add a new route /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Route to return a JSON about API status
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -11,3 +12,13 @@ def status():
     """Return a JSON with the API status"""
     data = {"status": "OK"}
     return jsonify(data)
+
+@app_views.route('/stats', strict_slashes=False)
+def count_all():
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                    })
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,23 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or None if not found
+            Parameters:
+                - cls: class
+                - id: string representing the object ID
+        """
+        if cls.__name__ in classes:
+            objs_cls = self.all(cls)
+            key = cls.__name__ + '.' + id
+            return (objs_cls.get(key))
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        return len(self.all(cls))
